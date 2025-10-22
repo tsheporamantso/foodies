68b2a628338709429f9dff71470760ec4401daef
tsheporamantso <tshepo.ramantso@outlook.com>
2025-10-22 10:24:57 +0200

    Remove unused Link imports and navigation links from HomePage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+// import Link from "next/link";
 
 export default function HomePage() {
   return (
@@ -9,11 +9,6 @@ export default function HomePage() {
       >
         Time to get started!
       </h1>
-      <p className="flex flex-col">
-        <Link href={"/meals"}>Meals Page</Link>
-        <Link href={"/meals/share"}>Share Meal</Link>
-        <Link href={"/community"}>Community Page</Link>
-      </p>
     </main>
   );
 }
