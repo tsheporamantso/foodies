d833fe12c0c385dc9c8c08769d02d3261176ee2a
tsheporamantso <tshepo.ramantso@outlook.com>
2025-10-22 10:24:49 +0200

    Add MainHeader component to RootLayout for improved navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import MainHeader from "@/components/MainHeader";
 
 export const metadata: Metadata = {
   title: "NextLevel Food",
@@ -35,7 +36,7 @@ export default function RootLayout({
             ></path>
           </svg>
         </div>
-
+        <MainHeader />
         {children}
       </body>
     </html>
