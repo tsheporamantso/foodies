2fec32274f83eedff479963c3242f2eea5317180
tsheporamantso <tshepo.ramantso@outlook.com>
2025-10-22 19:25:33 +0200

    Refactor MealsPage component to fetch and display meals with improved layout and styling

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,11 +1,28 @@
 import Link from "next/link";
-import React from "react";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/MealsGrid";
+import { getMeals } from "@/lib/meals";
 
-const MealsPage = () => {
+const MealsPage = async () => {
+  const meals = await getMeals();
   return (
     <>
-      <h1 className="text-3xl">MealsPage</h1>
-      <Link href={"/meals/something"}>Meals Slug</Link>
+      <header className={classes.header}>
+        <h1 className="text-5xl font-bold mb-6">
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          {" "}
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href={"/meals/share"}>Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
     </>
   );
 };
