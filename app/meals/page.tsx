8adadd935ba8be3ee45dfe8631479406addfef7a
tsheporamantso <tshepo.ramantso@outlook.com>
2025-10-23 06:54:41 +0200

    Created meals fetching component from DB and wrapped it with Suspense component for loading message when fetching data

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -2,9 +2,15 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/MealsGrid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+import MealsLoadingPage from "./loading-out";
 
-const MealsPage = async () => {
+const Meals = async () => {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+};
+
+const MealsPage = () => {
   return (
     <>
       <header className={classes.header}>
@@ -21,7 +27,9 @@ const MealsPage = async () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<MealsLoadingPage />}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
