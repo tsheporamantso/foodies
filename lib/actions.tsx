2be6b51dd4142fec0f644374c3696717e80f3ad8
tsheporamantso <tshepo.ramantso@outlook.com>
2025-10-23 19:22:36 +0200

    revalidated meals path to avoid aggressive caching

--- lib/actions.tsx
+++ lib/actions.tsx
@@ -2,6 +2,7 @@
 
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
+import { revalidatePath } from "next/cache";
 
 function isInvalidText(text: string | null | undefined) {
   return !text || text.trim() === "";
@@ -36,5 +37,6 @@ export async function shareMeal(
   }
 
   await saveMeal(meal);
+  revalidatePath("/meals");
   redirect("/meals");
 }
