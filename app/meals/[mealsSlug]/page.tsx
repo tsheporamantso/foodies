eb4a105d4727bd8155b3368a898b2b14019084da
tsheporamantso <tshepo.ramantso@outlook.com>
2025-10-23 10:12:01 +0200

    Fetch meal from databse and add dynamic route page

--- app/meals/[mealsSlug]/page.tsx
+++ app/meals/[mealsSlug]/page.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import Image from "next/image";
 
-const MealsDetailsPage = () => {
-  return <h1 className="text-3xl">MealsDetailsPage</h1>;
+type MealsDetailsPageProps = {
+  params: {
+    mealsSlug: string;
+  };
+};
+
+const MealsDetailsPage = async ({ params }: MealsDetailsPageProps) => {
+  const { mealsSlug } = await params;
+  const meal = getMeal(mealsSlug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill sizes="33vw" alt={meal.title} src={meal.image} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 };
 
 export default MealsDetailsPage;
